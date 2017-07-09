37437a4f65d25ca56d21a467111caad678e3a927
keke <542524698@qq.com>
2017-07-09 18:20:59 +0800

    optimize
    Signed-off-by: keke <542524698@qq.com>

--- oa_system/pages/confirm/confirm.js
+++ oa_system/pages/confirm/confirm.js
@@ -1,201 +1,212 @@
 const Api = require('../../utils/api.js')
 
 Page({
-  data: {
-    company:null,
-    checkButton: true,
-    showView2: true,
-    winWidth: '',
-    winHeight: '',
-    name: '_________________________',
-  },
+    data: {
+        company: null,
+        checkButton: true,
+        showView2: true,
+        winWidth: '',
+        winHeight: '',
+        name: '_________________________',
+    },
 
-  onLoad: function (options) {
-    var that = this
-    // 获取系统信息
-    this.setData({
-      id: options.companyId,
-      token: wx.getStorageSync('token'),
-      userid: wx.getStorageSync("userid")
-    })
-
-  },
-
-  onShow: function() {
-    this.getCompanyInfo()
-  },
-  //获取中间点的经纬度，并mark出来
-  getLngLat: function () {
-      var that = this;
-      this.mapCtx = wx.createMapContext("map4select");
-      this.mapCtx.getCenterLocation({
-          success: function (res) {
-              that.setData({
-                  longitude: res.longitude,
-                  latitude: res.latitude,
-                  markers: [
-                      {
-                          id: 0,
-                          iconPath: "../../images/ic_position.png",
-                          longitude: res.longitude,
-                          latitude: res.latitude,
-                          width: 30,
-                          height: 30,
-                      }
-                  ]
-              })
-          }
-      })
-  },
-  regionchange(e) {
-      // 地图发生变化的时候，获取中间点，也就是用户选择的位置
-      if (e.type == 'end') {
-          this.getLngLat()
-      }
-  },
-  getCompanyInfo: function() {
-      var that = this
-      //set the width and height
-      // 动态设置map的宽和高
-      wx.getSystemInfo({
-          success: function (res) {
-              console.log('getSystemInfo');
-              console.log('map_width:', res.windowWidth)
-              console.log('map_height:', res)
-              that.setData({
-                  map_width: res.windowWidth,
-                  map_height: res.windowHeight,
-                  /*
-                  controls: [{
-                      id: 1,
-                      iconPath: '../../images/ic_location.png',
-                      position: {
-                          left: res.windowWidth / 2 - 8,
-                          top: res.windowHeight / 2 - 16,
-                          width: 30,
-                          height: 30
-                      },
-                      clickable: true
-                  }]*/
-              })
-          }
-      })
-    
-    console.log("id:", this.data.id)
-    wx.request({
-      url: Api.companyDetail,  // + this.data.id + '?token=' + this.data.token,
-      data: {
-        token: this.data.token,
-        id: this.data.id
-      },
-      method: 'GET',
-      header:{
-        "Content-Type": "application/x-www-form-urlencoded"
-      },      
-
-      success: (res) => {
-        console.log("getcompany info return:", res)
+    onLoad: function (options) {
+        var that = this
+        // 获取系统信息
         this.setData({
-          company: res.data.data,
-          markers: [{
-              id: 0,
-              iconPath: "../../images/ic_position.png",
-              longitude: res.data.data.Longitude,
-              latitude: res.data.data.Latitude,
-              width: 30,
-              height: 30,
-          }]
+            id: options.companyId,
+            token: wx.getStorageSync('token'),
+            userid: wx.getStorageSync("userid")
         })
-      },
-      fail: function (fail) {
-        console.log(fail)
-      },
-    })
-  },
-  exit_commit: function(e){
-      var that= this
-      that.setData({
-          showView2: true,
-      }) 
-  },
 
-  commit: function(event){
-    var that = this
-    console.log("commit info: ", event)
-    var name = event.detail.value.name;
-    if(name.indexOf("_____") >= 0){
-        wx.showToast({
-            title: '请备注姓名',
-            icon: 'success',
-            duration: 2000
+    },
+
+    onShow: function () {
+        this.getCompanyInfo()
+    },
+    //获取中间点的经纬度，并mark出来
+    getLngLat: function () {
+        var that = this;
+        this.mapCtx = wx.createMapContext("map4select");
+        this.mapCtx.getCenterLocation({
+            success: function (res) {
+                that.setData({
+                    longitude: res.longitude,
+                    latitude: res.latitude,
+                    markers: [
+                        {
+                            id: 0,
+                            iconPath: "../../images/ic_position.png",
+                            longitude: res.longitude,
+                            latitude: res.latitude,
+                            width: 30,
+                            height: 30,
+                        }
+                    ]
+                })
+            }
         })
-        return 
-    }
-    console.log("name:", name)
-    wx.request({
-        url: Api.joinCompany,
-        data: {
-            token: that.data.token,
-            companyId: that.data.company.Id,
-            userid: that.data.userid,
-            username: name,
-        },
-        method: 'POST',
-        header: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        success: (res) => {
-            console.log(res)
-            wx.redirectTo({
-                url: '/pages/audit/audit?companyName=' + this.data.company.Name + '&message=已发送发送申请消息'
-            })
+    },
+    regionchange(e) {
+        // 地图发生变化的时候，获取中间点，也就是用户选择的位置
+        if (e.type == 'end') {
+            this.getLngLat()
         }
-    })
+    },
+    getCompanyInfo: function () {
+        var that = this
+        //set the width and height
+        // 动态设置map的宽和高
+        wx.getSystemInfo({
+            success: function (res) {
+                console.log('getSystemInfo');
+                console.log('map_width:', res.windowWidth)
+                console.log('map_height:', res)
+                that.setData({
+                    map_width: res.windowWidth,
+                    map_height: res.windowHeight,
+                    /*
+                    controls: [{
+                        id: 1,
+                        iconPath: '../../images/ic_location.png',
+                        position: {
+                            left: res.windowWidth / 2 - 8,
+                            top: res.windowHeight / 2 - 16,
+                            width: 30,
+                            height: 30
+                        },
+                        clickable: true
+                    }]*/
+                })
+            }
+        })
 
-  },
-  checkBtn_click: function (event) {
-    var that = this
-    /*
-    console.log("company id:", this.data.id)
-    var message = "您已申请加入 " + this.data.company.Name
-    var companyid = this.data.id
-    var content = "确定加入"+ this.data.company.Name +"吗？"
-    */
-    that.setData({
-        showView2: false,
-    })   
+        console.log("id:", this.data.id)
+        wx.request({
+            url: Api.companyDetail,  // + this.data.id + '?token=' + this.data.token,
+            data: {
+                token: this.data.token,
+                id: this.data.id
+            },
+            method: 'GET',
+            header: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
 
-    /*
-    wx.showModal({
-      title: '申请加入',
-      content: content,
-      success: (res) => {
-        if (res.confirm) {
-          wx.redirectTo({
-            url: '/pages/audit/audit?companyName=' + this.data.company.Name + '&message=' + message
-          })
-          
-          wx.request({
+            success: (res) => {
+                console.log("getcompany info return:", res)
+                if (res.data.Code == 200) {
+
+                    this.setData({
+                        company: res.data.data,
+                        markers: [{
+                            id: 0,
+                            iconPath: "../../images/ic_position.png",
+                            longitude: Number(res.data.data.Longitude),
+                            latitude: Number(res.data.data.Latitude),
+                            width: 30,
+                            height: 30,
+                        }]
+                    })
+                    wx.openLocation({
+                        latitude: Number(res.data.data.Latitude),
+                        longitude: Number(res.data.data.Longitude),
+                        name:res.data.data.Name,
+                        address: res.data.data.Address,
+                        infoUrl:'/pages/log/log'
+                    })
+                    console.log("=====================")
+                }
+            },
+            fail: function (fail) {
+                console.log(fail)
+            },
+        })
+    },
+    exit_commit: function (e) {
+        var that = this
+        that.setData({
+            showView2: true,
+        })
+    },
+
+    commit: function (event) {
+        var that = this
+        console.log("commit info: ", event)
+        var name = event.detail.value.name;
+        if (name.indexOf("_____") >= 0) {
+            wx.showToast({
+                title: '请备注姓名',
+                icon: 'success',
+                duration: 2000
+            })
+            return
+        }
+        console.log("name:", name)
+        wx.request({
             url: Api.joinCompany,
             data: {
-              token: this.data.token,
-              companyId: this.data.company.Id,
+                token: that.data.token,
+                companyId: that.data.company.Id,
+                userid: that.data.userid,
+                username: name,
             },
             method: 'POST',
             header: {
-              "Content-Type": "application/x-www-form-urlencoded"
+                "Content-Type": "application/x-www-form-urlencoded"
             },
             success: (res) => {
-              console.log(res)
-              wx.redirectTo({
-                url: '/pages/audit/audit?companyName=' + this.data.company.Name + '&message=已发送发送申请消息'
-             })
+                console.log(res)
+                wx.redirectTo({
+                    url: '/pages/audit/audit?companyName=' + this.data.company.Name + '&message=已发送发送申请消息'
+                })
             }
-          })
-        }
-      }
-    })*/
+        })
 
-  }
+    },
+    checkBtn_click: function (event) {
+        var that = this
+        /*
+        console.log("company id:", this.data.id)
+        var message = "您已申请加入 " + this.data.company.Name
+        var companyid = this.data.id
+        var content = "确定加入"+ this.data.company.Name +"吗？"
+        */
+        that.setData({
+            showView2: false,
+        })
+
+        /*
+        wx.showModal({
+          title: '申请加入',
+          content: content,
+          success: (res) => {
+            if (res.confirm) {
+              wx.redirectTo({
+                url: '/pages/audit/audit?companyName=' + this.data.company.Name + '&message=' + message
+              })
+              
+              wx.request({
+                url: Api.joinCompany,
+                data: {
+                  token: this.data.token,
+                  companyId: this.data.company.Id,
+                },
+                method: 'POST',
+                header: {
+                  "Content-Type": "application/x-www-form-urlencoded"
+                },
+                success: (res) => {
+                  console.log(res)
+                  wx.redirectTo({
+                    url: '/pages/audit/audit?companyName=' + this.data.company.Name + '&message=已发送发送申请消息'
+                 })
+                }
+              })
+            }
+          }
+        })*/
+
+    }
 
 })
--- oa_system/pages/list/list.js
+++ oa_system/pages/list/list.js
@@ -4,12 +4,17 @@ Page({
   data: {
     lists: [],
     winWidth: '',
-    winHeight: ''
+    winHeight: '',
+    showView2: true,
+    companyid: null,
+    companyname: null,
+    name: '_________________________',
   },
   onLoad: function () {
     //设置token
     this.setData({
-      token: wx.getStorageSync('token')
+      token: wx.getStorageSync('token'),
+      userid: wx.getStorageSync("userid")
     })
 
     // 获取系统信息 
@@ -55,6 +60,80 @@ Page({
         console.log(fail)
       },
     })
+  },
+
+  detail: function(event){
+      //var companyid = event.target.id
+      console.log(event)
+      wx.openLocation({
+          latitude: Number(event.currentTarget.dataset.lat),
+          longitude: Number(event.currentTarget.dataset.lon),
+          name: event.currentTarget.dataset.name,
+          address: event.currentTarget.dataset.add,
+          success: function(res){
+            console.log(res)
+          },
+          
+          fail: function(fail) {
+              console.log(fail)
+          },
+      })
+  },
+
+  join: function(event){
+    var that = this
+    console.log(event)
+    console.log("join action")
+    var companyid = event.currentTarget.dataset.id
+    var companyname = event.currentTarget.dataset.name
+    that.setData({
+        showView2: false,
+        companyid: companyid,
+        companyname: companyname,
+    })
+  },
+
+  exit_commit: function (e) {
+      var that = this
+      that.setData({
+          showView2: true,
+      })
+  },
+
+  commit: function (event) {
+      var that = this
+      console.log("commit info: ", event)
+      var name = event.detail.value.name;
+      console.log(name)
+      if (name.indexOf("_____") >= 0 || name == "") {
+          wx.showToast({
+              title: '请备注姓名',
+              icon: 'success',
+              duration: 2000
+          })
+          return
+      }
+      console.log("name:", name)
+      wx.request({
+          url: Api.joinCompany,
+          data: {
+              token: that.data.token,
+              companyId: that.data.companyid,
+              userid: that.data.userid,
+              username: name,
+          },
+          method: 'POST',
+          header: {
+              "Content-Type": "application/x-www-form-urlencoded"
+          },
+          success: (res) => {
+              console.log(res)
+              wx.redirectTo({
+                  url: '/pages/audit/audit?companyName=' + this.data.companyname + '&message=已发送发送申请消息'
+              })
+          }
+      })
+
   },
 
   //事件处理函数
